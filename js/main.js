0c8476d742fea28baff55b347d78d560d84b9879
Rapkowsky <robertrapkowski19@gmail.com>
2022-03-15 20:32:58 +0100

    Basic files structure added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let $todoInput; // miejsce, gdzie użytkownik wpisuje treść
+let $alertInfo; // info o braku zadań / konieczności dodania tekstu
+let $addBtn; // przycisk ADD - dodaje nowe elementy do listy
+let $ulList; // nasza lista zadań, tagi <ul></ul>
+let $newTask; // nowo dodany LI, nowe zadanie
+let $allTasks; // lista wszystkich dodanych LI
+let $idNumber = 0; // ID dodawane do każdego nowego zadania
+let $popup; //pobrany popup
+let $popupInfo; // alert w popupie, jak się doda pusty tekst
+let $editedTodo; // edytowany Todo
+let $popupInput; //tekst wpisywany w inputa w popup'ie
+let $addPopupBtn; // przycisk "zatwierdź" w popup'ie
+let $closeTodoBtn; //przycisk od zamykania popup'a
